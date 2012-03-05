38a67c43a328e49c3ced47a04140d404256e3124
Tedikova Olga <tedolga@gmail.com>
2012-03-05 16:54:00 +0400

    Reader was implemented in inner class

--- benchmark/src/main/java/benchmark/ThreadStarter.java
+++ benchmark/src/main/java/benchmark/ThreadStarter.java
@@ -11,25 +11,22 @@ public class ThreadStarter {
     public static void main(String[] args) {
         final Resource syncResource = new SynchronizedResource();
         final Resource concurrentResource = new ConcurrentResource();
+    }
 
-        Callable syncReader = new Callable<Long>() {
-            public Long call() throws Exception {
-                long startTime = System.currentTimeMillis();
-                syncResource.read(1000);
-                long endTime = System.currentTimeMillis();
-                return endTime - startTime;
-            }
-        };
+    private class Reader implements Callable {
+        private Resource resource;
+        private long readingTime;
 
-        Callable concurrentReader = new Callable<Long>() {
-            public Long call() throws Exception {
-                long startTime = System.currentTimeMillis();
-                concurrentResource.read(1000);
-                long endTime = System.currentTimeMillis();
-                return endTime - startTime;
-            }
-        };
+        public Reader(Resource resource, long readingTime) {
+            this.resource = resource;
+            this.readingTime = readingTime;
+        }
 
-
+        public Long call() throws Exception {
+            long startTime = System.currentTimeMillis();
+            resource.read(readingTime);
+            long endTime = System.currentTimeMillis();
+            return endTime - startTime;
+        }
     }
 }

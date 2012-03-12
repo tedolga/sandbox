f50a738c482df8c5e764993d59d2797162295581
Tedikova <tedolga@gmail.com>
2012-03-12 09:26:21 +0400

    update algoritm

--- VKCup/src/main/java/A/Friends.java
+++ VKCup/src/main/java/A/Friends.java
@@ -1,8 +1,6 @@
 package A;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @author Tedikova O.
@@ -22,20 +20,13 @@ public class Friends {
         for (int i = 0; i < n; i++) {
             String[] event = scanner.nextLine().split(" ");
             String[] users = new String[]{event[0], event[1]};
-            Message message = new Message(Long.parseLong(event[2]));
-            processNewMessage(users, message);
+            processNewMessage(users, Long.parseLong(event[2]));
         }
-
-
     }
 
     private static class Message {
         private long time;
         private byte relation;
-
-        public Message(long time) {
-            this.time = time;
-        }
 
         public long getTime() {
             return time;
@@ -55,49 +46,34 @@ public class Friends {
     }
 
     private static Message lookUp(String user1, String user2) {
-        if (chat.get(user1) != null) {
-            return chat.get(user1).get(user2);
-        } else {
-            return null;
+        if (chat.get(user1) == null) {
+            Map<String, Message> childMap = new HashMap<String, Message>();
+            childMap.put(user2, new Message());
+            chat.put(user1, childMap);
         }
+        return chat.get(user1).get(user2);
     }
 
-    private static String[] orderUsers(String[] users, Message message) {
-        String[] sortedUsers = new String[2];
-        if (users[0].compareTo(users[1]) <= 0) {
-            message.setRelation((byte) 1);
-            sortedUsers[0] = users[0];
-            sortedUsers[1] = users[1];
-        } else {
-            message.setRelation((byte) 2);
-            sortedUsers[0] = users[1];
-            sortedUsers[1] = users[0];
-        }
-        return sortedUsers;
-    }
-
-    private static void processNewMessage(String[] users, Message message) {
-        String[] sortedUsers = orderUsers(users, message);
-        String user1 = sortedUsers[0];
-        String user2 = sortedUsers[1];
-        Message savedMessage = lookUp(user1, user2);
-        if (savedMessage != null) {
-            byte resultMask = (byte) (savedMessage.getRelation() + message.getRelation());
-            if (message.getTime() - savedMessage.getTime() <= d && resultMask == (byte) 3) {
-                savedMessage.setRelation(resultMask);
-                System.out.println(user1 + " " + user2);
-            } else {
-                savedMessage.setRelation(message.getRelation());
+    private static void processNewMessage(String[] users, long time) {
+        byte mask = users[0].compareTo(users[1]) <= 0 ? (byte) 1 : (byte) 2;
+        Arrays.sort(users, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
             }
-            savedMessage.setTime(message.getTime());
-        } else {
-            putNewMessage(user1, user2, message);
+        });
+        String user1 = users[0];
+        String user2 = users[1];
+        Message savedMessage = lookUp(user1, user2);
+        boolean delta = (time - savedMessage.getTime()) <= d;
+
+        savedMessage.setRelation(delta ? (byte) (savedMessage.getRelation() | mask) : mask);
+        savedMessage.setTime(time);
+
+        if (savedMessage.getRelation() == 0x3) {
+            savedMessage.setRelation((byte) 0xFF);
+            System.out.println(user1 + " " + user2);
         }
     }
 
-    private static void putNewMessage(String user1, String user2, Message message) {
-        Map<String, Message> childMap = new HashMap<String, Message>();
-        childMap.put(user2, message);
-        chat.put(user1, childMap);
-    }
 }
